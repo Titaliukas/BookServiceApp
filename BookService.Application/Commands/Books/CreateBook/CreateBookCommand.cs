95f9dc6f3f961bc88ae87b1e098163bbf27b6a25
Titaliukas <rimtitas@gmail.com>
2024-10-29 23:27:44 +0200

    Command: Create Book

---
+++ BookService.Application/Commands/Books/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace BookService.Application.Commands.Books.CreateBook;
+
+public record CreateBookCommand(string Title, string Year) : IRequest<int>;
---
+++ BookService.Application/Commands/Books/CreateBook/CreateBookCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using BookService.Domain.Entities;
+using BookService.Infrastructure;
+using MediatR;
+
+namespace BookService.Application.Commands.Books.CreateBook;
+
+public class CreateBookCommandHandler : IRequestHandler<CreateBookCommand, int>
+{
+    private readonly BooksDbContext _booksDbcontext;
+    
+    public CreateBookCommandHandler(BooksDbContext booksDbContext)
+    {
+        _booksDbcontext = booksDbContext;
+    }
+    
+    public async Task<int> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+    {
+        var book = new Book
+        {
+            Title = request.Title,
+            Year = Convert.ToInt32(request.Year),
+            CreateDate = DateTime.Now.ToUniversalTime()
+        };
+        
+        await _booksDbcontext.Books.AddAsync(book, cancellationToken);
+        await _booksDbcontext.SaveChangesAsync(cancellationToken);
+        
+        return book.Id;
+    }
+}
