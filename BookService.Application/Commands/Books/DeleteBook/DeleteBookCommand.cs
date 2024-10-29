794196ee993950d1c415820f83da3c98de308913
Titaliukas <rimtitas@gmail.com>
2024-10-29 23:27:31 +0200

    Command: Delete Book

---
+++ BookService.Application/Commands/Books/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace BookService.Application.Commands.Books.DeleteBook;
+
+public record DeleteBookCommand(int Id) : IRequest<Unit>;
---
+++ BookService.Application/Commands/Books/DeleteBook/DeleteBookCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using BookService.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookService.Application.Commands.Books.DeleteBook;
+
+public class DeleteBookCommandHandler : IRequestHandler<DeleteBookCommand, Unit>
+{
+    private readonly BooksDbContext _booksDbContext;
+
+    public DeleteBookCommandHandler(BooksDbContext booksDbContext)
+    {
+        _booksDbContext = booksDbContext;
+    }
+    
+    public async Task<Unit> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+    {
+        var BookToDelete = 
+            await _booksDbContext.Books
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        if (BookToDelete == null)
+        {
+            throw new Exception();
+        }
+        
+        _booksDbContext.Books.Remove(BookToDelete);
+        await _booksDbContext.SaveChangesAsync(cancellationToken);
+        
+        return Unit.Value;
+    }
+}
