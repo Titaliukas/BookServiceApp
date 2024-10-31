e929a342b7a401bf53de3b0fff6e7178e986151f
Titaliukas <rimtitas@gmail.com>
2024-10-31 16:42:53 +0200

    ignore

--- BookService.Presentation/obj/Debug/net8.0/BookService.Presentation.AssemblyInfo.cs
+++ BookService.Presentation/obj/Debug/net8.0/BookService.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookService.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ab30ee3598211db12dd21370953cf514ecb754")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b746e747edb43a8322fe47346032a52efca833")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookService.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookService.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
